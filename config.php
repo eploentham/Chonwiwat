1a04ff56a086bca1a528342f52b2987ec4f04638
Ekapop <eploentham@gmail.com>
2017-06-01 08:07:09 +0700

    open cart
    room5

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/chonwiwat/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/chonwiwat/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/chonwiwat/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/chonwiwat/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/chonwiwat/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/chonwiwat/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/chonwiwat/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/chonwiwat/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/chonwiwat/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/chonwiwat/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/chonwiwat/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/chonwiwat/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/chonwiwat/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'chonwiwat');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
